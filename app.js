66b879a6ed9b0b719311f8a0ec0db8df835d4343
오현석 <ohs0939@gmail.com>
2018-08-10 03:24:46 +0900

    beverage manager

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require( './routes/index' );
+var users = require( './routes/users' );
+var beverages = require( './routes/beverages' );
 
 var app = express();
+
+app.locals.pretty = true;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,8 +27,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use( '/', routes );
+app.use( '/users', users );
+app.use( '/beverage', beverages );
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/beverages.js
@@ -0,0 +1,47 @@
+﻿'use strict';
+var express = require( 'express' );
+var router = express.Router();
+
+/* GET home page. */
+router.get( '/', function ( req, res ) {
+    res.render( 'index', { title: 'Express' } );
+} );
+
+router.get( '/edit', function ( req, res ) {
+
+	res.render( 'beverage_edit' );
+
+} );
+
+router.post( '/add', function ( req, res ) {
+
+	console.log( req.body );
+
+	var msg = '';
+
+	for( var key in req.body )
+	{
+		msg += key + ' : ' + req.body[key] + '<br>';
+	}
+
+	res.send( 'this is add manager<br><br>' + msg );
+
+} );
+
+router.post( '/del', function ( req, res ) {
+
+	console.log( req.body );
+
+	var msg = '';
+
+	for( var key in req.body ) {
+		msg += key + ' : ' + req.body[key] + '<br>';
+	}
+
+	res.send( 'this is del manager<br><br>' + msg );
+
+
+} );
+
+
+module.exports = router;
