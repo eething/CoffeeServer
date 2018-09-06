addbec6fb40861e787b656030386cb9c35cc7412
eething <ohs0939@gmail.com>
2018-09-06 16:32:26 +0900

    order fetch 작업중 리팩토링

--- public/javascripts/vieworder.js
+++ public/javascripts/vieworder.js
@@ -35,105 +35,12 @@ l2order = {
 		}
 	},
 
-	cbOrderList() {
-
-		function makeOptionStr( option ) {
-			let optionStr = '';
-			for( const k in option ) {
-				if( k === 'icehot' ) {
-					const icehot = (( option[k] === 'ice' ) ? '아이스' :
-									( option[k] === 'hot' ) ? '따뜻' : '' );
-					optionStr = icehot +
-								( ( icehot && optionStr ) ? '/' : '' ) +
-								optionStr;
-				} else if( k === 'syrup' ) {
-					optionStr += ( optionStr ? '/' : '' ) +
-								( option[k] === 'minus' ? '시럽빼고' : '' );
-				}
-			}
-			return optionStr;
-		}
-
-		removeChildAll( elem.tableOrderB );
-		removeChildAll( elem.tableOrderO );
-
-		l2data.currentOrder.forEach( co => {
-			let optionStr = makeOptionStr( co );
-			const user = l2data.allUsers[co.orderBy];
-			let username = '';
-			if( user ) {
-				username = user.name ? user.name : user.id;
-			} else {
-				username = `* ${co.orderBy}`
-			}
-			let tr = addElement( elem.tableOrderO, 'tr', 'cOrderItem' );
-
-			const beverageStr = `${co.beverage}(${optionStr})`;
-			let size = Math.min( Math.max( 30 - beverageStr.length, 11 ), 15 );
-
-			addElement( tr, 'td', 'cOrderBy', username );
-			let td = addElement( tr, 'td', 'cBeverageOption', beverageStr );
-			td.style.fontSize = size + 'px';
-		} );
-
-		let totalCount = 0;
-		for( const k of l2data.buyKeysSorted ) {
-			for( const v of l2data.currentBuy[k] ) {
-				let tr = addElement( elem.tableOrderB, 'tr', 'cOrderItem' );
-
-				let tdBeverage = addElement( tr, 'td', 'cBeverage', k );
-				let size = Math.min( Math.max( 23 - k.length * 2, 11 ), 15 );
-				tdBeverage.style.fontSize = size + 'px';
-
-				const option = JSON.parse( v.options );
-				let optionStr = makeOptionStr( option );
-				let tdOption = addElement( tr, 'td', 'cOption', optionStr );
-				size = Math.min( Math.max( 25 - optionStr.length, 10 ), 15 );
-				tdOption.style.fontSize = size + 'px';
-
-				let tdNum = addElement( tr, 'td' );
-				let aPopup = addElement( tdNum, 'a', '', v.orderBys.length );
-				totalCount += v.orderBys.length;
-				function popupViewOrderBys() {
-					removeChildAll( elem.divPopup );
-					addElement( elem.divPopup, 'p', '', k );
-					addElement( elem.divPopup, 'p', '', optionStr );
-					v.orderBys.forEach( o => {
-						let inner = '';
-						const user = l2data.allUsers[o];
-						if( user ) {
-							inner = user.name ? user.name : user.id;
-						} else {
-							inner = `* ${o}`
-						}
-						addElement( elem.divPopup, 'p', '', inner );
-					} );
-
-					let input = addElement( elem.divPopup, 'input', 'cNadoNado', '' );
-					input.type = 'button';
-					input.value = '나도나도';
-					input.addEventListener( 'click', function ( event ) {
-						selectBeverage( k );
-						checkOptions( option );
-					} );
-
-					showPopup( true );
-				}
-				aPopup.addEventListener( 'click', function ( event ) {
-					popupViewOrderBys();
-				} );
-			}
-		}
-
-		document.querySelector( 'span.cTotal' ).innerHTML = `총 ${totalCount} 잔`;
-	},
-
 	cbUserList() {
 		let select = document.querySelector( '#orderBy' );
 		removeChildAll( select );
 
 		for( const uid in l2data.allUsers ) {
-			let user = l2data.allUsers[uid];
+			let user = l2data.allUsers[ uid ];
 			let option = addElement( select, 'option', '',
 				user.name ? user.name : user.id );
 			option.value = uid;
@@ -141,6 +48,119 @@ l2order = {
 
 		l2data.getCurrentOrderList( l2order.cbOrderList );
 	},
+
+	_makeOptionStr( option ) {
+		let optionStr = '';
+		for( const k in option ) {
+			if( k === 'icehot' ) {
+				const icehot = (( option[k] === 'ice' ) ? '아이스' :
+								( option[k] === 'hot' ) ? '따뜻' : '' );
+				optionStr = icehot +
+							( ( icehot && optionStr ) ? '/' : '' ) +
+							optionStr;
+			} else if( k === 'syrup' ) {
+				optionStr += ( optionStr ? '/' : '' ) +
+							( option[k] === 'minus' ? '시럽빼고' : '' );
+			}
+		}
+		return optionStr;
+	},
+	_addTableOrderO( co ) {
+		let optionStr = this._makeOptionStr( co );
+		const user = l2data.allUsers[co.orderBy];
+		let username = '';
+		if( user ) {
+			username = user.name ? user.name : user.id;
+		} else {
+			username = `* ${co.orderBy}`
+		}
+		let tr = addElement( elem.tableOrderO, 'tr', 'cOrderItem' );
+
+		const beverageStr = `${co.beverage}(${optionStr})`;
+		let size = Math.min( Math.max( 30 - beverageStr.length, 11 ), 15 );
+
+		addElement( tr, 'td', 'cOrderBy', username );
+		let td = addElement( tr, 'td', 'cBeverageOption', beverageStr );
+		td.style.fontSize = size + 'px';
+	},
+	_makeTableOrderO() {
+		removeChildAll( elem.tableOrderO );
+		l2data.currentOrder.forEach( co => {
+			this._addTableOrderO( co );
+		} );
+	},
+	_totalCount: 0,
+	_addPopup( tdNum, k, v, optionStr ) {
+		let aPopup = addElement( tdNum, 'a', '', v.orderBys.length );
+		this._totalCount += v.orderBys.length;
+		function popupViewOrderBys() {
+			removeChildAll( elem.divPopup );
+			addElement( elem.divPopup, 'p', '', k );
+			addElement( elem.divPopup, 'p', '', optionStr );
+			v.orderBys.forEach( o => {
+				let inner = '';
+				const user = l2data.allUsers[o];
+				if( user ) {
+					inner = user.name ? user.name : user.id;
+				} else {
+					inner = `* ${o}`
+				}
+				addElement( elem.divPopup, 'p', '', inner );
+			} );
+
+			let input = addElement( elem.divPopup, 'input', 'cNadoNado', '' );
+			input.type = 'button';
+			input.value = '나도나도';
+			input.addEventListener( 'click', function ( event ) {
+				selectBeverage( k );
+				checkOptions( option );
+			} );
+
+			showPopup( true );
+		}
+		aPopup.addEventListener( 'click', function ( event ) {
+			popupViewOrderBys();
+		} );
+	},
+	_addTableOrderB( k, v ) {
+		let tr = addElement( elem.tableOrderB, 'tr', 'cOrderItem' );
+
+		let tdBeverage = addElement( tr, 'td', 'cBeverage', k );
+		let size = Math.min( Math.max( 23 - k.length * 2, 11 ), 15 );
+		tdBeverage.style.fontSize = size + 'px';
+
+		const option = JSON.parse( v.options );
+		let optionStr = this._makeOptionStr( option );
+		let tdOption = addElement( tr, 'td', 'cOption', optionStr );
+		size = Math.min( Math.max( 25 - optionStr.length, 10 ), 15 );
+		tdOption.style.fontSize = size + 'px';
+
+		let tdNum = addElement( tr, 'td' );
+		this._addPopup( tdNum, k, v, optionStr );
+	},
+	_setTotalForOrderB() {
+		document.querySelector( 'span.cTotal' ).innerHTML = `총 ${this._totalCount} 잔`;
+	},
+	_makeTableOrderB() {
+		removeChildAll( elem.tableOrderB );
+		this._totalCount = 0;
+		for( const k of l2data.buyKeysSorted ) {
+			for( const v of l2data.currentBuy[k] ) {
+				this._addTableOrderB( k, v );
+			}
+		}
+	},
+
+	cbOrderList() {
+		l2order._makeTableOrderO();
+		l2order._makeTableOrderB();
+		l2order._setTotalForOrderB();
+	},
+	cbOrderOne( order ) {
+		l2order._addTableOrderO( order );
+		l2order._addTableOrderB( '엄훠', order );
+		l2order._setTotalForOrderB();
+	}
 }
 
 function swapOrderList( self ) {
@@ -177,17 +197,22 @@ function addOrder( self ) {
 		return;
 	}
 
-
 	const data = {
+		orderBy: f.orderBy.value,
 		beverage: f.beverage.value,
-		icehot: f.icehot.value,
-		syrup: f.syrup.value
 	}
-	f.beverage.value = '',
-	elem.radioIce.checked = false;
-	elem.radioHot.checked = false;
-	elem.chkSyrup.checked = false;
-	onChangeBeverage()
+	f.beverage.value = '';
+	if( f.icehot.value ) {
+		data.icehot = f.icehot.value;
+		elem.radioIce.checked = false;
+		elem.radioHot.checked = false;
+	}
+	if( f.syrup.checked ) {
+		data.syrup = f.syrup.value;
+		elem.chkSyrup.checked = false;
+	}
+
+	changeBeverage( f, '' );
 
 	fetchHelper( '/order', data, 'addOrder', data => {
 		if( data.code == 'OK' ) {
@@ -197,10 +222,6 @@ function addOrder( self ) {
 		}
 	} );
 }
-
-
-
-
 
 function checkOptions( option ) {
 	elem.radioIce.checked = (option.icehot === 'ice');
@@ -244,16 +265,22 @@ function showPopup( start ) {
 	document.querySelector( 'div.cDimmer' ).style.visibility = visMode;
 	elem.divPopupOuter.style.visibility = visMode;
 }
-function onChangeBeverage( value ) {
 
-	// TODO - self, trim;
+function onChangeBeverage( self ) {
+	const f = self.form;
+	if( self.type === 'text' ) {
+		self.value.trim();
+	}
+	changeBeverage( f, self.value );
+}
+function changeBeverage( f, value ) {
 
 	let bShow = false;
-	let beverage = l2data.allBeverages[value];
+	let beverage = l2data.allBeverages[ value ];
 	if( beverage ) {
 		bShow = true;
 	} else if( value === "" ) {
-		document.querySelector( '#beverage' ).style.backgroundColor = '';
+		f.beverage.style.backgroundColor = '';
 	} else {
 		removeChildAll( elem.divPopup );
 		let bFound = false;
@@ -277,14 +304,14 @@ function onChangeBeverage( value ) {
 			elem.divPopup.prepend( p );
 			showPopup( true );
 		} else {
-			document.querySelector( '#beverage' ).style.backgroundColor = 'red';
+			f.beverage.style.backgroundColor = 'red';
 		}
 	}
 
 	if( bShow ) {
 		elem.divOrderList.style.display = 'none';
 		showBeverageOptions( beverage );
-		document.querySelector( '#beverage' ).style.backgroundColor = 'lightgreen';
+		f.beverage.style.backgroundColor = 'lightgreen';
 	} else {
 		elem.divOrderList.style.display = 'block';
 		elem.divIce.style.display = 'none'
@@ -296,6 +323,7 @@ function onChangeBeverage( value ) {
 function selectBeverage( name ) {
 	document.querySelector( '#beverageSelect' ).value = name;
 	document.querySelector( '#beverage' ).value = name;
-	onChangeBeverage( name );
+	const order_form = document.querySelector( '#order_form' );
+	changeBeverage( order_form, name );
 	showPopup( false );
 }
